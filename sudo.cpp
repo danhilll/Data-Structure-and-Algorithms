cfae592d90743b1b918530139091fcd778a22b44
danhilll <nashdanhilll@gmail.com>
2024-12-04 11:35:27 +0800

    Add files via upload

---
+++ sudo.cpp
@@ -0,0 +1,40 @@
+START
+
+If I woke up <= 8:
+print("Make Coffee, Tea")
+
+elif coffee, tea is made == 8:05:
+print("Cook Egg, Fried Rice")
+
+else:
+print("Too lazy to eat")
+
+ENDIF
+
+if I woke up <= 5/-8:
+print("Make tea")
+
+elif == if:
+print("Buy bread (Pandesal/Monay)")
+
+elif == if:
+print("Cook egg or hotdog for the bread filling")
+
+elif == if:
+print("Get a glass of water")
+
+elif == if:
+print("Eat until full")
+
+elif == if:
+print("Brush my teeth")
+
+elif == if:
+print("Done")
+
+else:
+print("Wash the dishes, Do chores, Do pending Activities,")
+
+END
+
+ 
